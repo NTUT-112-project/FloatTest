6dce34e03b834e9acb69b52424f81a9b96cd33bb
James-Lu-none <jamesabcde277@gmail.com>
2024-09-22 21:44:06 +0800

    add overlay permission check and request flow

--- app/src/main/java/com/example/floattest/MainActivity.kt
+++ app/src/main/java/com/example/floattest/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.floattest
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,9 +15,10 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
 import com.example.floattest.databinding.ActivityMainBinding
 import com.hjq.window.EasyWindow
-
+const val REQUEST_CODE_OVERLAY_PERMISSION = 1000
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -31,15 +37,17 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener {
-            print("Floating Action Button Clicked");
-            EasyWindow.with(application) // 'this' refers to the current Activity
-                .setDraggable()
-                .setContentView(R.layout.toast_hint)
-                .setText(android.R.id.message, "点我消失")
-                .setOnClickListener(android.R.id.message, EasyWindow.OnClickListener<TextView?> { easyWindow, view ->
-                    easyWindow.cancel()
-                })
-                .show()
+            if(checkOverlayPermission(this)){
+                print("Floating Action Button Clicked");
+                EasyWindow.with(application) // 'this' refers to the current Activity
+                    .setDraggable()
+                    .setContentView(R.layout.toast_hint)
+                    .setText(android.R.id.message, "lol")
+                    .setOnClickListener(android.R.id.message, EasyWindow.OnClickListener<TextView?> { easyWindow, view ->
+                        easyWindow.cancel()
+                    })
+                    .show()
+            }
         }
     }
 
@@ -64,4 +72,20 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+
+    fun checkOverlayPermission(activity: Activity): Boolean {
+        if (!Settings.canDrawOverlays(activity)) {
+            // If permission is not granted, show a message and navigate to settings
+            Toast.makeText(activity, "Overlay permission is required", Toast.LENGTH_SHORT).show()
+
+            // Create an intent to navigate to the overlay permission settings page
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:${activity.packageName}")
+            )
+            activity.startActivityForResult(intent, REQUEST_CODE_OVERLAY_PERMISSION)
+        }
+        return Settings.canDrawOverlays(activity)
+    }
+
 }
